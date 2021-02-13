80176866325efb8f31f0bc629facfd98f3c879fa
ANDRI YABU NDAPA REHING <andriyabu@gmail.com>
2021-02-13 18:14:44 +0700

    Add Rspec API testing for GET, POST and DELETE function

--- spec/requests/book_spec.rb
+++ spec/requests/book_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
-describe 'GET /books', type: :request do
-  it 'returns all books' do
-    FactoryBot.create(:book, title: 'Cook Book', author:'Andri Yabu')
-    FactoryBot.create(:book, title: 'Baristha Book', author:'Andri Yabu')
-    get '/api/v1/books'
+describe 'Books API', type: :request do
+  describe 'GET /books' do
+    it 'returns all books' do
+      FactoryBot.create(:book, title: 'Cook Book', author:'Andri Yabu')
+      FactoryBot.create(:book, title: 'Baristha Book', author:'Andri Yabu')
+      get '/api/v1/books'
 
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(2)
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
+
+  describe 'POST /books' do
+    it 'create a new book' do
+      expect {
+        post '/api/v1/books', params: {book: {title: 'New cook book', author: 'Aladin'}}
+      }.to change { Book.count }.from(0).to(1)
+
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe 'DELETE /books/:id' do
+    let(:book) {FactoryBot.create(:book, title: 'Cook Book', author:'Andri Yabu')}
+    it 'deletes a book' do
+      delete "/api/v1/books/#{book.id}"
+
+      expect(response).to have_http_status(:no_content)
+    end
   end
 end
